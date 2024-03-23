77a8f4d85319bc514b21d4c0bfb29f0a3a10d0cf
s25179 <s25179@pjwstk.edu.pl>
2024-03-23 22:57:40 +0100

    Add method to find average value in array

--- aplikacja/Program.cs
+++ aplikacja/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World111222333!");
+﻿using System;
+
+public class Statistics
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta.");
+        }
+
+        long sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        return (double)sum / numbers.Length;
+    }
+}
